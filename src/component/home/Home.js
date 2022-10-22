a15ac4e5b6b9435e08e645c20690390151583cc1
ginohmk <michotall95@gmail.com>
2022-10-22 05:30:06 +0100

    core: Add folder for Home This folder is a component with its own style

---
+++ src/component/home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HomeContainer } from './style/Home.style';
+
+const Home = () => {
+  return <HomeContainer>Hello</HomeContainer>;
+};
+
+export default Home;
---
+++ src/component/home/style/Home.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  width: 90%;
+  max-width: 1152px;
+  margin: 0 auto;
+`;
