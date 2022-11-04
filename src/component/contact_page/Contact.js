504930e20f3d0111c290951e5a0afb50bf9aad14
ginohmk <michotall95@gmail.com>
2022-11-04 10:42:06 +0100

    feat: Set proper width for contact section wrapper

--- src/component/contact_page/Contact.js
+++ src/component/contact_page/Contact.js
@@ -4,61 +4,63 @@ import { ContactContainer } from './Contact.style';
 const Contact = () => {
   return (
     <ContactContainer>
-      <h2>Contact Me</h2>
+      <div className="contact-wrapper">
+        <h2>Contact Me</h2>
 
-      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
 
-      <div className="formContainer">
-        <form>
-          <div className="username">
-            <div className="input-container">
-              <label>First name</label>
-              <input
-                type="text"
-                id="first_name"
-                placeholder="Enter your first name"
-              />
+        <div className="formContainer">
+          <form>
+            <div className="username">
+              <div className="input-container">
+                <label>First name</label>
+                <input
+                  type="text"
+                  id="first_name"
+                  placeholder="Enter your first name"
+                />
+              </div>
+
+              <div className="input-container">
+                <label>Last name</label>
+                <input
+                  type="text"
+                  id="last_name"
+                  placeholder="Enter your last name"
+                />
+              </div>
             </div>
 
             <div className="input-container">
-              <label>Last name</label>
-              <input
-                type="text"
-                id="last_name"
-                placeholder="Enter your last name"
+              <label>Email</label>
+              <input type="text" id="email" placeholder="yourname@email.com" />
+            </div>
+
+            <div className="textarea-container input-container">
+              <label>Message</label>
+              <textarea
+                id="message"
+                placeholder="Send me a message and I'll reply you as soon as possible..."
               />
             </div>
-          </div>
 
-          <div className="input-container">
-            <label>Email</label>
-            <input type="text" id="email" placeholder="yourname@email.com" />
-          </div>
-
-          <div className="textarea-container input-container">
-            <label>Message</label>
-            <textarea
-              id="message"
-              placeholder="Send me a message and I'll reply you as soon as possible..."
-            />
-          </div>
-
-          <div className="checkbox">
-            <div className="check">
-              <input type="checkbox" name="agreement" />
+            <div className="checkbox">
+              <div className="check">
+                <input type="checkbox" name="agreement" />
+              </div>
+              <p className="agreement">
+                You agree to providing your data to &#123;name&#125; who may
+                contact you.
+              </p>
             </div>
-            <p className="agreement">
-              You agree to providing your data to &#123;name&#125; who may
-              contact you.
-            </p>
-          </div>
 
-          <div className="btn-container">
-            <button type="submit" className="btn">
-              Send message
-            </button>
-          </div>
-        </form>
+            <div className="btn-container">
+              <button type="submit" className="btn">
+                Send message
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </ContactContainer>
   );
--- src/component/contact_page/Contact.style.js
+++ src/component/contact_page/Contact.style.js
@@ -5,135 +5,142 @@ export const ContactContainer = styled.div`
   max-width: 1152px;
   margin: 0 auto;
   padding-top: 4rem;
-  margin-bottom: 11.75rem;
+  margin-bottom: 7.75rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-  h2 {
-    color: #101828;
-    font-weight: 600;
-    font-size: 2.25rem;
-    line-height: 44px;
-    margin-bottom: 1.2rem;
-  }
+  .contact-wrapper {
+    width: 100%;
+    h2 {
+      color: #101828;
+      font-weight: 600;
+      font-size: 2.25rem;
+      line-height: 44px;
+      margin-bottom: 1.2rem;
+    }
 
-  p {
-    color: #475467;
-    font-size: 1rem;
-    line-height: 30px;
-    font-weight: 400;
-    margin-bottom: 3rem;
-  }
+    p {
+      color: #475467;
+      font-size: 1rem;
+      line-height: 30px;
+      font-weight: 400;
+      margin-bottom: 3rem;
+    }
 
-  .formContainer {
-    form {
-      display: flex;
-      flex-direction: column;
-      gap: 1.5rem;
-      .username {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: 2rem;
-        width: 100%;
-        .input-container {
-          width: 100%;
-          input {
-            width: 100%;
-          }
-        }
-      }
-
-      .input-container {
+    .formContainer {
+      form {
         display: flex;
         flex-direction: column;
-        align-items: flex-start;
-        gap: 0.375rem;
-
-        label {
-          font-size: 0.7rem;
-          font-weight: 500;
-          line-height: 20px;
-          color: #344054;
-          font-weight: 500;
+        gap: 1.5rem;
+        .username {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          gap: 2rem;
+          width: 100%;
+          .input-container {
+            width: 100%;
+            input {
+              width: 100%;
+            }
+          }
         }
 
-        input {
-          border: 1px solid #d0d5dd;
-          box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-          border-radius: 8px;
-          background: #ffffff;
-          padding: 10px 14px;
+        .input-container {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          gap: 0.375rem;
 
-          width: 100%;
-          height: 44px;
-
-          &:focus {
-            outline: none;
-            border: 1px solid #84caff;
+          label {
+            font-size: 0.7rem;
+            font-weight: 500;
+            line-height: 20px;
+            color: #344054;
+            font-weight: 500;
           }
 
-          &::placeholder {
-            color: #667085;
+          input {
+            border: 1px solid #d0d5dd;
+            box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+            border-radius: 8px;
+            background: #ffffff;
+            padding: 10px 14px;
+
+            width: 100%;
+            height: 44px;
+
+            &:focus {
+              outline: none;
+              border: 1px solid #84caff;
+            }
+
+            &::placeholder {
+              color: #667085;
+              font-weight: 400;
+              font-size: 0.7rem;
+              line-height: 24px;
+            }
+          }
+        }
+
+        .textarea-container {
+          width: 100%;
+
+          textarea {
+            width: 100%;
+            height: 132px;
+            background: #ffffff;
+            border: 1px solid #d0d5dd;
+            box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+            border-radius: 8px;
+            padding: 12px 14px;
+
+            &:focus {
+              outline: none;
+              border: 1px solid #84caff;
+            }
+          }
+        }
+
+        .checkbox {
+          display: flex;
+          gap: 0.75rem;
+          align-items: flex-start;
+
+          .check {
+            margin-top: -1.5px;
+          }
+
+          .agreement {
+            font-size: 0.8rem;
+            color: #475467;
             font-weight: 400;
-            font-size: 0.7rem;
             line-height: 24px;
           }
         }
-      }
-
-      .textarea-container {
-        width: 100%;
-
-        textarea {
+        .btn-container {
           width: 100%;
-          height: 132px;
-          background: #ffffff;
-          border: 1px solid #d0d5dd;
-          box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-          border-radius: 8px;
-          padding: 12px 14px;
-
-          &:focus {
-            outline: none;
-            border: 1px solid #84caff;
+          height: 48px;
+          .btn {
+            border: none;
+            color: #fff;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            padding: 12px 20px;
+            gap: 8px;
+            width: 100%;
+            background: #1570ef;
+            background: #1570ef;
+            box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+            border-radius: 8px;
+            cursor: pointer;
+            font-size: 1rem;
           }
-        }
-      }
-
-      .checkbox {
-        display: flex;
-        gap: 0.75rem;
-        align-items: flex-start;
-
-        .check {
-          margin-top: -1.5px;
-        }
-
-        .agreement {
-          font-size: 0.8rem;
-          color: #475467;
-          font-weight: 400;
-          line-height: 24px;
-        }
-      }
-      .btn-container {
-        width: 100%;
-        height: 48px;
-        .btn {
-          border: none;
-          color: #fff;
-          display: flex;
-          flex-direction: row;
-          justify-content: center;
-          align-items: center;
-          padding: 12px 20px;
-          gap: 8px;
-          width: 100%;
-          background: #1570ef;
-          background: #1570ef;
-          box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
-          border-radius: 8px;
-          cursor: pointer;
-          font-size: 1rem;
         }
       }
     }
